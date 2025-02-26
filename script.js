b1090b66be807cf361a5355477e7fe99b46e11b7
SheebaZaim <sheeba.zaim@gmail.com>
2025-02-27 00:17:35 +0500

    first commit

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const nav = document.getElementById('navbar');
 
 if (bar) {
     bar.addEventListener('click', () => {
-        nav.classList.add('active');  // Toggle instead of just adding
+        nav.classList.add('active');  // 
     });
 }
 if (close) {
     close.addEventListener('click', () => {
-        nav.classList.remove('active');  // Toggle instead of just adding
+        nav.classList.remove('active');  // 
     });
 }
